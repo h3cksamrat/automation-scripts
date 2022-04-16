d53dc159475b2b196ef8dec65c11c1317723fcbb
h3cksamrat (mint) <samratneupane215@gmail.com>
2022-04-16 12:17:18 +0545

    command line number generator

---
+++ numGenerator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void numberGen(int start, int stop);
+
+int main(int argc, char* argv[])
+{
+   switch (argc){
+      case 1:
+         printf("Error: Expected atleast one argument\nnumGen <start> <stop> or numGen <stop>\n");
+         break;
+      case 2:
+         numberGen(0, atoi(argv[1]));
+         break;
+      case 3:
+         numberGen(atoi(argv[1]), atoi(argv[2]));
+         break;
+      default:
+         printf("Too many arguments passed\n");
+   }
+}
+
+void numberGen(int start, int stop) {
+   if (start == stop) return;
+   if (start < stop)
+      for (int i = start; i < stop; i++)
+         printf("%d\n", i);
+   if (start > stop)  
+      for (int i = start; i > stop; i--)
+         printf("%d\n", i);
+}
