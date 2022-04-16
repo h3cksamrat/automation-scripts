ba601a983f1dc89ca79b7f56ddb4aa66ddbde27d
h3cksamrat (mint) <samratneupane215@gmail.com>
2022-04-16 15:04:21 +0545

    optimized code

--- numGenerator.c
+++ numGenerator.c
@@ -3,33 +3,30 @@
 
 void numberGen(int start, int stop);
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-   switch (argc){
-      case 1:
-         printf("Error: Expected atleast one argument\nnumGen <start> <stop> or numGen <stop>\n");
-         break;
-      case 2:
-         numberGen(0, atoi(argv[1]));
-         break;
-      case 3:
-         numberGen(atoi(argv[1]), atoi(argv[2]));
-         break;
-      default:
-         printf("Too many arguments passed\n");
+   switch (argc)
+   {
+   case 1:
+      printf("Error: Expected atleast one argument\nnumGen <start> <stop> or numGen <stop>\n");
+      break;
+   case 2:
+      numberGen(0, atoi(argv[1]));
+      break;
+   case 3:
+      numberGen(atoi(argv[1]), atoi(argv[2]));
+      break;
+   default:
+      printf("Too many arguments passed\n");
    }
 }
 
-void numberGen(int start, int stop) {
-   if (start == stop) return;
-   switch (start < stop) {
-      case 0:
-         for (int i = start; i > stop; i--)
-            printf("%d\n", i);
-         break;
-      case 1:
-         for (int i = start; i < stop; i++)
-            printf("%d\n", i);
-         break;
-   }
+void numberGen(int start, int stop)
+{
+   if (start < stop)
+      for (int i = start; i > stop; i--)
+         printf("%d\n", i);
+   else
+      for (int i = start; i < stop; i++)
+         printf("%d\n", i);
 }
